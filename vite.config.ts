602dfa25d94915f9d9d5d35abf076e02cb74c140
ironcandle1208 <ironcandle1208@gmail.com>
2025-09-18 22:41:22 +0900

    modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, //同じネットワーク内の他のデバイスからの接続を許可
+    port: 5173, //開発サーバが使用するポート番号
+  }
 })
