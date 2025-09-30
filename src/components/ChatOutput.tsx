3d3e8c6c05154f39961d75aea55c0486c8249c09
ironcandle1208 <ironcandle1208@gmail.com>
2025-09-30 22:51:01 +0900

    生成された語呂を削除できるように変更、tailwind設定ファイルを追加

--- src/components/ChatOutput.tsx
+++ src/components/ChatOutput.tsx
@@ -2,22 +2,24 @@ import CustomButton from "./CustomButton";
 import type { ChatOutputProps } from "../props/ChatProps";
 
 //　出力欄コンポーネント
-const ChatOutput = ({ isLoading, isComplete }: ChatOutputProps) => {
+const ChatOutput = ({ isLoading, text, onDelete }: ChatOutputProps) => {
   return (
-    <div className="flex justify-center h-full overflow-y-auto">
+    <div className="flex justify-center overflow-y-auto">
       {isLoading && (
         <div className="animate-spin h-6 w-6 border-3 border-gray-500 rounded-full border-t-transparent"></div>
       )}
-      {isComplete && (
-        <div className="flex flex-col">
-          <CustomButton className="my-1 text-black bg-gray-200 hover:bg-gray-200/60 cursor-pointer">
-            OUTPUT
-          </CustomButton>
-          <CustomButton className="my-1 text-black bg-gray-200 hover:bg-gray-200/60 cursor-pointer">
-            OUTPUT
+      <div className="flex flex-col">
+        <div className="my-1 py-1 px-1 text-black bg-gray-200 items-center rounded-md">
+          {text || (isLoading ? "生成中..." : "ERROR")}
+          <CustomButton
+            name="delete"
+            className="ml-2 px-1 pt-1 text-black bg-gray-300 hover:bg-gray-200/60 cursor-pointer"
+            onClick={onDelete}
+          >
+            <span className="i-proicons-delete text-black inline-block size-5 cursor-pointer"></span>
           </CustomButton>
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- src/props/ChatProps.tsx
+++ src/props/ChatProps.tsx
@@ -6,11 +6,19 @@ export type LoadingSetter = Dispatch<SetStateAction<boolean>>;
 export interface ChatInputProps {
   isLoading: boolean;
   setIsLoading: LoadingSetter;
-  setIsComplete: LoadingSetter;
+  onSubmit: (text: string) => Promise<void>;
 }
 
 //　ChatOutPut用Props
 export interface ChatOutputProps {
   isLoading: boolean;
-  isComplete: boolean;
+  text: string;
+  onDelete: () => void;
+}
+
+// ChatOutput要素
+export interface ChatOutputItem {
+  id: string;
+  isLoading: boolean;
+  text: string;
 }
